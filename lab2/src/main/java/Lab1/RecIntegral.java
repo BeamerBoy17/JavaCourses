180e35ef217a6953c04375dd44473a8015b4e106
BeamerBoy17 <72580478+BeamerBoy17@users.noreply.github.com>
2022-03-01 23:45:13 +0300

    Add files via upload

---
+++ lab2/src/main/java/Lab1/RecIntegral.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab1;
+
+public class RecIntegral{
+    public double upperEdge;
+    public double lowerEdge;
+    public double step;
+    
+    RecIntegral(double upperEdge,double lowerEdge,double step){
+        this.upperEdge = upperEdge;
+        this.lowerEdge = lowerEdge;
+        this.step = step;
+    }
+}
+
